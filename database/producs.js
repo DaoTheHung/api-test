e7400952db15f09f065972994b5183b1eaa1c8ed
DaoTheHung <Daohung447@gmail.com>
2023-03-15 14:41:51 +0700

    update data

---
+++ database/producs.js
@@ -0,0 +1,106 @@
+const product = [
+    {
+        id: 1,
+        name: "Kids Headphones",
+        price: "850.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product1a_600x.jpg?v=1596259840",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product1b-Version1_600x.jpg?v=1596259858",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product1d.jpg?v=1596259858",
+
+    },
+    {
+        id: 2,
+        name: "Light Headphones",
+        price: "880.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product2a_600x.jpg?v=1596259890",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product2c_Version1_600x.jpg?v=1596259905",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product2d.jpg?v=1596259908",
+
+    },
+    {
+        id: 3,
+        name: "On-Ear Headphones",
+        price: "750.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product3a_600x.jpg?v=1596259963",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product3c_Version1_600x.jpg?v=1596259975",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/Product3d.jpg?v=1596259977",
+
+    },
+    {
+        id: 4,
+        name: "Leather Headset",
+        price: "910.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-42_713214f6-ce84-43f2-adba-7a13a882ead3_600x.jpg?v=1595414449",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-44_6a471e3a-e3bf-4760-a207-1883a4e30dcf.jpg?v=1595414449",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-46_c1267b6c-75f2-4972-8187-4005be2c9060.jpg?v=1595414449",
+    },
+    {
+        id: 5,
+        name: "BT Headset",
+        price: "910.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-1_631b5b8e-1aa8-43de-ae6a-cb74b0e2462e_600x.jpg?v=1595414385",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-4_d4473519-8847-44ff-b320-215ed44f0739_600x.jpg?v=1595414385",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-6_af294e14-5550-4b4a-ae7d-1202e8ae2c6b.jpg?v=1595414385",
+
+    },
+    {
+        id: 6,
+        name: "Closed Headset",
+        price: "910.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-37_020016e0-ddb3-4d67-8e5a-d42378ef9d00.jpg?v=1595414441",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-39_e73afc4e-b246-4c83-8207-68b105703781_600x.jpg?v=1595414441",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-41_8c210410-9eb0-4933-a336-605fbe2af6eb.jpg?v=1595414441",
+
+    },
+    {
+        id: 7,
+        name: "Commando Headset",
+        price: "860.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-79_b56b789a-ec45-4f21-b119-bd42588f4e87_600x.jpg?v=1595414501",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-81_507b9dee-5e8d-4f10-8256-495f33a79508_600x.jpg?v=1595414501",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-83_d394931c-9168-42e9-b18c-0c10be6a2950.jpg?v=1595414501",
+
+    },
+    {
+        id: 8,
+        name: "HP BTP1 Headset",
+        price: "910.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-27_1aaae500-ff30-40e6-a4a0-0caddfebe780.jpg?v=1595414425",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-29_422e2e6d-e8fe-4435-b91b-26de6cc9ed1a.jpg?v=1595414425",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-31_28cdd27f-0d39-403a-b666-a9b8fca49a1b.jpg?v=1595414425",
+
+    },
+    {
+        id: 9,
+        name: "HP Delux Headset",
+        price: "910.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-57_600x.jpg?v=1595414471",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-60_600x.jpg?v=1595414471",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-62_607bc09b-fdd1-42aa-8d06-efabe9450477.jpg?v=1595414471",
+
+    },
+    {
+        id: 10,
+        name: "HP Gaming Headset",
+        price: "910.00",
+        quantity: 1,
+        image: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-7_4448e6c4-243a-4b81-8009-765baaa6ec64_600x.jpg?v=1595414392",
+        imageSlick1: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-9_b8e4188f-8af9-4ad9-b598-d8443fc55e06_600x.jpg?v=1595414392",
+        imageSlick2: "//cdn.shopify.com/s/files/1/0434/2520/2335/products/shop-11_cb37283b-c5ff-401c-b137-7e3a0fba0ff0.jpg?v=1595414392",
+
+    },
+
+]
+
+module.exports = {
+    product,
+}
